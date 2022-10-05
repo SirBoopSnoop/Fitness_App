fcb191a74fa2aefc019c4836ee05b081263e3317
SirBoopSnoop <60667105+SirBoopSnoop@users.noreply.github.com>
2022-10-05 19:21:49 +0200

    Navbar progress

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -2,17 +2,46 @@ package com.example.testapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Variables
+    lateinit var toolbar: Toolbar
+    lateinit var navigationView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnOpenActivity.setOnClickListener {
+        /*btnOpenActivity.setOnClickListener {
             Intent(this, SecondActivity::class.java).also{
                 startActivity(it)
             }
-        }
+        }*/
+
+        /*--------------------------Hooks--------------------------------*/
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val NavigationView = findViewById<NavigationView>(R.id.nav_view)
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        /*--------------------------Tool Bar-----------------------------*/
+
+        setSupportActionBar(toolbar)
+
+        /*-------------------------Navigation Drawer Menu--------------------------*/
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
     }
+
 }
