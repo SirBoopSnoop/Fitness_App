e1d53e0476de74c08d77cd6d2f286741d1a463a6
SirBoopSnoop <60667105+SirBoopSnoop@users.noreply.github.com>
2022-10-11 21:57:53 +0200

    starting to add a creation screen

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -2,14 +2,16 @@ package com.example.testapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     //Variables
     lateinit var toolbar: Toolbar
@@ -20,15 +22,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*btnOpenActivity.setOnClickListener {
-            Intent(this, SecondActivity::class.java).also{
+        create_button.setOnClickListener {
+            Intent(this, CreateActivity::class.java).also{
                 startActivity(it)
             }
-        }*/
+        }
 
         /*--------------------------Hooks--------------------------------*/
         toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val NavigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView = findViewById<NavigationView>(R.id.nav_view)
         drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
 
         /*--------------------------Tool Bar-----------------------------*/
@@ -36,12 +38,28 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         /*-------------------------Navigation Drawer Menu--------------------------*/
-
+        navigationView.bringToFront()
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        navigationView.setNavigationItemSelectedListener(this)
 
+    }
+
+    override fun onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+
+        super.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 
 }
