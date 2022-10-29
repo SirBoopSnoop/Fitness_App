f2fba7bb2780f04f7586e101209aaff1480ef343
ThomasML <thomasml@hiof.no>
2022-10-29 23:22:25 +0200

    Fixed the radio group multiple selected bug and changed the layout for CreateActivity

--- app/src/main/java/com/example/testapplication/CreateActivity.kt
+++ app/src/main/java/com/example/testapplication/CreateActivity.kt
@@ -1,10 +1,16 @@
 package com.example.testapplication
 
+import android.R
 import android.os.Bundle
-import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testapplication.databinding.ActivityCreateBinding
+import kotlinx.android.synthetic.main.activity_create.*
 import kotlinx.android.synthetic.main.activity_create.view.*
+import kotlinx.android.synthetic.main.header.*
+
 
 class CreateActivity : AppCompatActivity() {
 
@@ -20,6 +26,34 @@ class CreateActivity : AppCompatActivity() {
         var intensityValue = binding.intensityValue.intensity_value
         var breakTimeValue = binding.breakTimeValue.break_time_value*/
 
+/*        @Override
+        fun onClick(view: View) {
+            val radioId: Int = binding.categoryGroup.checkedRadioButtonId
+
+            val radioButton = findViewById<RadioButton>(radioId)
+
+            textView2.setText("Your choice: " + radioButton.text)
+        }*/
+
+        category_group.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener { group, checkedId ->
+                val radio: RadioButton = findViewById(checkedId)
+                Toast.makeText(applicationContext," On checked change :"+
+                        " ${radio.text}",
+                    Toast.LENGTH_SHORT).show()
+            })
+
+
+
+
         binding.cancelButton.setOnClickListener {finish()}
     }
+
+/*    fun ActivatedButton(view: View) {
+        val radioId: Int = binding.categoryGroup.checkedRadioButtonId
+
+        val radioButton = findViewById<RadioButton>(radioId)
+
+        Toast.makeText(this@CreateActivity, "Selected exercise category: " + radioButton.text, Toast.LENGTH_SHORT).show()
+    }*/
 }
